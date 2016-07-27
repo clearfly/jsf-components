4dc63c63c6cf163d4785d27a04ad783b1969d6cb
Cody Lerum <cody.lerum@gmail.com>
2016-07-27 11:35:52 -0600

    use clipboard.js for copy component

--- src/main/java/com/outjected/jsf/renderers/CopyRenderer.java
+++ src/main/java/com/outjected/jsf/renderers/CopyRenderer.java
@@ -2,9 +2,6 @@ package com.outjected.jsf.renderers;
 
 import java.io.IOException;
 
-import javax.faces.application.Resource;
-import javax.faces.application.ResourceDependencies;
-import javax.faces.application.ResourceDependency;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
@@ -14,14 +11,10 @@ import com.outjected.jsf.components.Famlies;
 import com.outjected.jsf.utils.RendererTools;
 
 @SuppressWarnings("resource")
-@ResourceDependencies({ @ResourceDependency(name = "ZeroClipboard.min.js", library = "com.outjected.jsf.core") })
 @FacesRenderer(componentFamily = Famlies.OUTPUT_COMPONENT_FAMILY, rendererType = CopyRenderer.RENDERER_TYPE)
 public class CopyRenderer extends RendererBase {
 
     public static final String RENDERER_TYPE = "com.outjected.jsf.renderers.CopyRenderer";
-
-    private boolean closeNeeded;
-    private String divId = null;
 
     @Override
     public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
@@ -34,40 +27,24 @@ public class CopyRenderer extends RendererBase {
             else {
                 value = objectValue.toString();
             }
-            divId = component.getClientId();
-            closeNeeded = true;
             ResponseWriter writer = context.getResponseWriter();
-            writer.startElement("div", component);
+            writer.startElement("button", component);
+            writer.writeAttribute("type", "button", "type");
             writeId(context, component);
-
-            // Write styleClass
-            String copyClass = (String) component.getAttributes().getOrDefault("copyClass", "icon-copy");
-            String styleClass = (String) component.getAttributes().get("styleClass");
-            String styleClassValue = RendererTools.spaceSeperateStrings("clipboard-copy-div", copyClass, styleClass);
+            String styleClass = (String) component.getAttributes().getOrDefault("styleClass", "btn btn-default btn-xs");
+            String styleClassValue = RendererTools.spaceSeperateStrings("clipboard-copy-div", styleClass);
             writeAttribute("class", styleClassValue, context);
             writeAttribute("data-clipboard-text", value, context);
+            writeAttribute("data-clipboard-action", "copy", context);
 
+            writer.startElement("i", component);
+            writer.writeAttribute("class", "fa fa-copy", "class");
+            writer.endElement("i");
+            writer.endElement("button");
         }
     }
 
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
-        if (closeNeeded) {
-            ResponseWriter writer = context.getResponseWriter();
-            writer.endElement("div");
-            writer.startElement("script", component);
-            writer.writeAttribute("type", "text/javascript", null);
-
-            Resource resource = context.getApplication().getResourceHandler().createResource("ZeroClipboard.swf", "com.outjected.jsf.core");
-            String resourcePath = resource != null ? resource.getRequestPath() : "Missing";
-            String copyClass = (String) component.getAttributes().getOrDefault("copyClass", "icon-copy");
-            String copiedClass = (String) component.getAttributes().getOrDefault("copied", "icon-paste");
-
-            String content = String.format("startZclip('%s','%s', '%s', '%s');", divId, resourcePath, copyClass, copiedClass);
-
-            writer.write(content);
-            writer.endElement("script");
-        }
     }
-
 }
