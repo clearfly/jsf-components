91ebccac493958828983745b00df2e2db2b72c85
Cody Lerum <cody.lerum@gmail.com>
2019-05-17 16:49:46 -0600

    don't render panel if having no renderable children

--- src/main/java/com/outjected/jsf/components/TabsComponent.java
+++ src/main/java/com/outjected/jsf/components/TabsComponent.java
@@ -1,24 +1,33 @@
 package com.outjected.jsf.components;
 
-import javax.faces.component.FacesComponent;
-import javax.faces.component.UIComponentBase;
-
 import com.outjected.jsf.renderers.PanelRenderer;
 
-@FacesComponent(value = "com.outjected.jsf.components.TabsComponent", namespace = Famlies.NAMESPACE)
-public class TabsComponent extends UIComponentBase {
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIComponentBase;
+
+@FacesComponent(value = "com.outjected.jsf.components.TabsComponent", namespace = Famlies.NAMESPACE) public class TabsComponent extends UIComponentBase {
 
     public TabsComponent() {
         setRendererType(PanelRenderer.RENDERER_TYPE);
     }
 
-    @Override
-    public String getFamily() {
+    @Override public String getFamily() {
         return Famlies.OUTPUT_COMPONENT_FAMILY;
     }
 
-    @Override
-    public boolean getRendersChildren() {
+    @Override public boolean getRendersChildren() {
         return true;
+    }
+
+    @Override public boolean isRendered() {
+        if (!super.isRendered()) {
+            //If explicitly set to not render then don't render
+            return false;
+        }
+        else {
+            //Only Render if some of the children are set to render
+            return getChildren().stream().filter(c -> c instanceof TabComponent).anyMatch(UIComponent::isRendered);
+        }
     }
 }
