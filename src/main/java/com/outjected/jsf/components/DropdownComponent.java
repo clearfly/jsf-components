efd67fe8b0f836f1ec761de71253ac30e51c642e
Cody Lerum <cody.lerum@gmail.com>
2020-06-05 11:48:39 -0600

    don't render dropdown component if empty

--- src/main/java/com/outjected/jsf/components/DropdownComponent.java
+++ src/main/java/com/outjected/jsf/components/DropdownComponent.java
@@ -1,9 +1,11 @@
 package com.outjected.jsf.components;
 
 import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponent;
 import javax.faces.component.UIComponentBase;
 
 import com.outjected.jsf.renderers.DropdownRenderer;
+import com.outjected.jsf.utils.RendererTools;
 
 @FacesComponent(value = "com.outjected.jsf.components.DropdownComponent", namespace = Famlies.NAMESPACE)
 public class DropdownComponent extends UIComponentBase {
@@ -15,5 +17,12 @@ public class DropdownComponent extends UIComponentBase {
     @Override
     public String getFamily() {
         return Famlies.OUTPUT_COMPONENT_FAMILY;
+    }
+
+    @Override
+    public boolean isRendered() {
+        final boolean hasChildrenToRender = getChildren().stream().anyMatch(UIComponent::isRendered);
+        final boolean renderEmpty = RendererTools.attributeValueAsBoolean(getAttributes().get("renderEmpty"), false);
+        return hasChildrenToRender || renderEmpty;
     }
 }
