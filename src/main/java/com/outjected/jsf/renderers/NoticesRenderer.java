e84738d7833966c4471b487051a52c941a1683c5
Cody Lerum <cody.lerum@gmail.com>
2020-07-30 16:51:35 -0600

    fix dangling div on notices component

--- src/main/java/com/outjected/jsf/renderers/NoticesRenderer.java
+++ src/main/java/com/outjected/jsf/renderers/NoticesRenderer.java
@@ -2,6 +2,7 @@ package com.outjected.jsf.renderers;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -25,45 +26,49 @@ public class NoticesRenderer extends RendererBase {
         final String warningTitle = (String) component.getAttributes().getOrDefault("warningTitle", "Has Warnings -- Click for Details");
         final boolean expanded = RendererTools.attributeValueAsBoolean(component.getAttributes().get("expanded"), true);
 
-        ResponseWriter writer = context.getResponseWriter();
-        writer.startElement("div", component);
-        writeId(context, component);
         if (notices.isContainsNotices()) {
-            writer.startElement("div", component);
+            component.getAttributes().put(WRITE_CLOSING_KEY, true);
+
+            ResponseWriter writer = context.getResponseWriter();
+            writer.startElement("div", component); //Start Outer div
+            writeId(context, component);
+
+            writer.startElement("div", component); // Start Content div
             writer.writeAttribute("onclick", "$('#noticesContentDiv').slideToggle('slow'); $('#noticesTitleDiv').slideToggle('slow');", null);
 
             // Title Div
-            writer.startElement("div", component);
+            writer.startElement("div", component); //Start title div
             writer.writeAttribute("id", "noticesTitleDiv", null);
             writer.writeAttribute("class", notices.containsErrors() ? "alert alert-danger alert-thin text-center" : "alert alert-warning alert-thin text-center", null);
             writer.writeAttribute("style", "display:" + (expanded ? "none" : "block") + ";", null);
             writer.startElement("strong", component);
             writer.write(notices.containsErrors() ? errorTitle : warningTitle);
             writer.endElement("strong");
-
             writer.endElement("div"); // End Title Div
 
-            // Content Div
-            writer.startElement("div", component);
+            writer.startElement("div", component);  // Start Sections div
             writer.writeAttribute("id", "noticesContentDiv", null);
             writer.writeAttribute("style", "display:" + (expanded ? "block" : "none") + ";", null);
-
             writeSection(context, component, notices.getErrors(), "alert alert-thin alert-danger");
             writeSection(context, component, notices.getWarnings(), "alert alert-thin alert-warning");
+            writer.endElement("div"); // End Sections Div
+
             writer.endElement("div"); // End Content Div
         }
     }
 
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
-        ResponseWriter writer = context.getResponseWriter();
-        writer.endElement("div");
+        final boolean writeClosingDiv = (boolean) component.getAttributes().getOrDefault(WRITE_CLOSING_KEY, false);
+        if (writeClosingDiv) {
+            ResponseWriter writer = context.getResponseWriter();
+            writer.endElement("div");
+        }
     }
 
     private void writeSection(FacesContext context, UIComponent component, List<String> messages, String styleClass) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
-
-        if (messages != null && messages.size() > 0) {
+        if (Objects.nonNull(messages) && !messages.isEmpty()) {
             writer.startElement("div", component);
             writer.writeAttribute("class", styleClass, null);
             writer.startElement("ul", component);
