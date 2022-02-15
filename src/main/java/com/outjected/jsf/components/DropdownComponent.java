87ebb8bf712fab9bf379efcb6d1f436abc35b47e
Cody Lerum <cody.lerum@gmail.com>
2022-02-15 12:57:40 -0700

    convert modal and dropdown

--- src/main/java/com/outjected/jsf/components/DropdownComponent.java
+++ src/main/java/com/outjected/jsf/components/DropdownComponent.java
@@ -1,18 +1,17 @@
 package com.outjected.jsf.components;
 
+import java.io.IOException;
+import java.util.Objects;
+
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponent;
-import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 
-import com.outjected.jsf.renderers.DropdownRenderer;
 import com.outjected.jsf.utils.RendererTools;
 
 @FacesComponent(value = "com.outjected.jsf.components.DropdownComponent", namespace = Families.NAMESPACE)
-public class DropdownComponent extends UIComponentBase {
-
-    public DropdownComponent() {
-        setRendererType(DropdownRenderer.RENDERER_TYPE);
-    }
+public class DropdownComponent extends ComponentBase {
 
     @Override
     public String getFamily() {
@@ -29,5 +28,45 @@ public class DropdownComponent extends UIComponentBase {
         else {
             return false;
         }
+    }
+
+    @Override
+    public void encodeBegin(FacesContext context) throws IOException {
+        final ResponseWriter writer = context.getResponseWriter();
+        final UIComponent toggleFacet = getFacet("toggle");
+        final boolean alignRight = RendererTools.attributeValueAsBoolean(getAttributes().get("alignRight"), true);
+        final boolean positionStatic = RendererTools.attributeValueAsBoolean(getAttributes().get("positionStatic"), false);
+
+        if (Objects.isNull(toggleFacet)) {
+            throw new IllegalArgumentException("Toggle Facet must be defined");
+        }
+
+        final String styleClass = (String) getAttributes().get("styleClass");
+        final String positionClass = positionStatic ? "position-static" : null;
+        final String divComputedStyleClass = RendererTools.spaceSeperateStrings("dropdown", positionClass, styleClass);
+
+        writer.startElement("div", this);
+        writer.writeAttribute("class", divComputedStyleClass, null);
+
+        toggleFacet.encodeAll(context);
+
+        writer.startElement("div", this);
+
+        if (alignRight) {
+            writer.writeAttribute("class", "dropdown-menu dropdown-menu-right", null);
+        }
+        else {
+            writer.writeAttribute("class", "dropdown-menu", null);
+        }
+
+        writer.writeAttribute("aria-labelledby", "dropdownMenuButton", null);
+
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        writer.endElement("div"); // dropdown-menu div
+        writer.endElement("div"); // dropdown div
     }
 }
